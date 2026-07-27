0966f29bd4c563d27c16defe7e6757101785856c
Levan Labartkava <levanlabartkava2@gmail.com>
2026-07-27 13:16:49 +0100

    feat(auth): complete A1 firebase configuration foundation

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+  show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -12,7 +14,22 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 /// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    return web;
+    if (kIsWeb) {
+      return web;
+    }
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+      case TargetPlatform.linux:
+      case TargetPlatform.fuchsia:
+        throw UnsupportedError(
+          'FirebaseOptions have not been configured for this platform. '
+          'Run flutterfire configure after native platform files are present.',
+        );
+    }
   }
 
   static const FirebaseOptions web = FirebaseOptions(
